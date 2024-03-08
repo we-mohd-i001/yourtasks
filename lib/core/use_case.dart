0debe12486676598f215dbc46284a82bb9d3330d
Mohd Imran <mohd-i001@webreinvent.com>
2024-03-08 11:30:29 +0530

    added use case

--- lib/core/use_case.dart
+++ lib/core/use_case.dart
@@ -1,6 +1,34 @@
 import 'package:either_dart/either.dart';
+import 'package:equatable/equatable.dart';
+import 'package:yourtasks/domain/entities/task_collection.dart';
+import 'package:yourtasks/domain/entities/unique_id.dart';
 import 'package:yourtasks/domain/failures/failures.dart';
 
-abstract class UseCase<Type, Params>{
+abstract class UseCase<Type, Params> {
   Future<Either<Failure, Type>> call(Params params);
+}
+
+abstract class Params extends Equatable {}
+
+class NoParams extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class CollectionIdParam extends Params {
+  final CollectionId collectionId;
+
+  CollectionIdParam({required this.collectionId}) : super();
+
+  @override
+  List<Object?> get props => [collectionId];
+}
+
+class TaskCollectionParam extends Params {
+  final TaskCollection taskCollection;
+
+  TaskCollectionParam({required this.taskCollection}) : super();
+
+  @override
+  List<Object?> get props => [taskCollection];
 }
