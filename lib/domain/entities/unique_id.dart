64631e51491d17289e5317c2e1bc1b89961000a8
Mohd Imran <mohd-i001@webreinvent.com>
2024-03-08 11:21:22 +0530

    added unique id for collections

---
+++ lib/domain/entities/unique_id.dart
@@ -0,0 +1,28 @@
+import 'package:equatable/equatable.dart';
+import 'package:uuid/uuid.dart';
+
+class UniqueId with EquatableMixin{
+  const UniqueId._(this.value);
+
+  final String value;
+
+  factory UniqueId(){
+    return UniqueId._(const Uuid().v4());
+  }
+
+  factory UniqueId.fromUniqueString(String uniqueString{
+    return UniqueId._(uniqueString);
+  }
+
+  @override
+  List<Object?> get props => [value];
+
+}
+
+class CollectionId extends UniqueId{
+  const CollectionId._(String value) : super._(value);
+
+  factory CollectionId(){
+    return CollectionId._(const Uuid().v4());
+  }
+}
